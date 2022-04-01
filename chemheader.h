313b0a42254434f5f8dacf55e6d6b982ea88e1a6
rynsp8 <rynsp8@gmail.com>
2022-04-01 13:07:51 -0400

    adding char pointer for prefixes

--- chemheader.h
+++ chemheader.h
@@ -1,7 +1,8 @@
-//I chose a buffer size of 318 because that is the total amount of bytes of the headers
+// I chose a buffer size of 318 because that is the total amount of bytes of the headers
 #define BUFFER_SIZE 318
 
-typedef struct table{
+typedef struct table
+{
     int atomicNumber;
     char element[14];
     char symbol[13];
@@ -32,7 +33,10 @@ typedef struct table{
     int valence;
 } table;
 
-table *buildTable(char* file){
+table *element;
+
+table *buildTable(char *file)
+{
 
     table *element = malloc(118 * sizeof(table));
     char buffer[BUFFER_SIZE];
@@ -40,131 +44,162 @@ table *buildTable(char* file){
     int field_count = 0;
     int i = 0;
 
-    FILE* periodic;
+    FILE *periodic;
 
     periodic = fopen(file, "r");
 
-    while(fgets(buffer, BUFFER_SIZE, periodic) != NULL){
+    while (fgets(buffer, BUFFER_SIZE, periodic) != NULL)
+    {
         row_count++;
 
-        if(row_count == 1){
+        if (row_count == 1)
+        {
             continue;
         };
 
-        char* tmp = strdup(buffer);
-        char* token = strtok_r(tmp, ",", &tmp);
+        char *tmp = strdup(buffer);
+        char *token = strtok_r(tmp, ",", &tmp);
 
-        while(token != NULL){
+        while (token != NULL)
+        {
             field_count++;
 
-            if(field_count == 1){
+            if (field_count == 1)
+            {
                 element[i].atomicNumber = atoi(token);
             }
 
-            if(field_count == 2){
+            if (field_count == 2)
+            {
                 strcpy(element[i].element, token);
             }
 
-            if(field_count == 3){
+            if (field_count == 3)
+            {
                 strcpy(element[i].symbol, token);
             }
 
-            if(field_count == 4){
+            if (field_count == 4)
+            {
                 element[i].atomicMass = atof(token);
             }
 
-            if(field_count == 5){
+            if (field_count == 5)
+            {
                 element[i].neutrons = atoi(token);
             }
-            if(field_count == 6){
+            if (field_count == 6)
+            {
                 element[i].protons = atoi(token);
             }
 
-            if(field_count == 7){
+            if (field_count == 7)
+            {
                 element[i].electrons = atoi(token);
             }
 
-            if(field_count == 8){
+            if (field_count == 8)
+            {
                 element[i].period = atoi(token);
             }
 
-            if(field_count == 9){
+            if (field_count == 9)
+            {
                 element[i].group = atoi(token);
             }
 
-            if(field_count == 10){
+            if (field_count == 10)
+            {
                 strcpy(element[i].phase, token);
             }
 
-            if(field_count == 11){
+            if (field_count == 11)
+            {
                 element[i].radioactive = atoi(token);
             }
 
-            if(field_count == 12){
+            if (field_count == 12)
+            {
                 element[i].natural = atoi(token);
             }
 
-            if(field_count == 13){
+            if (field_count == 13)
+            {
                 element[i].metal = atoi(token);
             }
 
-            if(field_count == 14){
+            if (field_count == 14)
+            {
                 element[i].nonMetal = atoi(token);
             }
 
-            if(field_count == 15){
+            if (field_count == 15)
+            {
                 element[i].metalloid = atoi(token);
             }
 
-            if(field_count == 16){
+            if (field_count == 16)
+            {
                 strcpy(element[i].type, token);
             }
 
-            if(field_count == 17){
+            if (field_count == 17)
+            {
                 element[i].atomicRadius = atof(token);
             }
 
-            if(field_count == 18){
+            if (field_count == 18)
+            {
                 element[i].electronNegativity = atof(token);
             }
 
-            if(field_count == 19){
+            if (field_count == 19)
+            {
                 element[i].firstIonization = atof(token);
             }
 
-            if(field_count == 20){
+            if (field_count == 20)
+            {
                 element[i].density = atof(token);
             }
 
-            if(field_count == 21){
+            if (field_count == 21)
+            {
                 element[i].meltingPoint = atof(token);
             }
 
-            if(field_count == 22){
+            if (field_count == 22)
+            {
                 element[i].boilingPoint = atof(token);
             }
 
-            if(field_count == 23){
+            if (field_count == 23)
+            {
                 element[i].isotopes = atoi(token);
             }
 
-            if(field_count == 24){
+            if (field_count == 24)
+            {
                 strcpy(element[i].discoverer, token);
             }
 
-            if(field_count == 25){
+            if (field_count == 25)
+            {
                 element[i].year = atoi(token);
             }
 
-            if(field_count == 26){
+            if (field_count == 26)
+            {
                 element[i].specificHeat = atof(token);
             }
 
-            if(field_count == 27){
+            if (field_count == 27)
+            {
                 element[i].shells = atoi(token);
             }
 
-            if(field_count == 28){
+            if (field_count == 28)
+            {
                 element[i].valence = atoi(token);
             }
 
@@ -177,3 +212,63 @@ table *buildTable(char* file){
     return element;
     fclose(periodic);
 };
+
+void printtable(struct table *element)
+{
+
+    for (int j = 0; j < 118; j++)
+    {
+        printf("%d\t%s\t\t%s\n", element[j].atomicNumber, element[j].element, element[j].symbol);
+    }
+}
+
+void printElement(char *input)
+{
+
+    printf("Enter printElement\n");
+    printf("%s\n", input);
+    printf("%s\n", element[0].element);
+
+    for (int i = 0; i < 118; i++)
+    {
+        printf("%s\n", element[i].element);
+        if (strcmp(input, element[i].element) == 0)
+        {
+            printf("%d %s %s %.2f %d %d %d %d %d %s %d %d %d %d %d %s %f %f %f %f %f %f %d %s %d %f %d %d\n",
+                   element[i].atomicNumber,
+                   element[i].element[14],
+                   element[i].symbol[13],
+                   element[i].atomicMass,
+                   element[i].neutrons,
+                   element[i].protons,
+                   element[i].electrons,
+                   element[i].period,
+                   element[i].group,
+                   element[i].phase[11],
+                   element[i].radioactive,
+                   element[i].natural,
+                   element[i].metal,
+                   element[i].nonMetal,
+                   element[i].metalloid,
+                   element[i].type[21],
+                   element[i].atomicRadius,
+                   element[i].electronNegativity,
+                   element[i].firstIonization,
+                   element[i].density,
+                   element[i].meltingPoint,
+                   element[i].boilingPoint,
+                   element[i].isotopes,
+                   element[i].discoverer[31],
+                   element[i].year,
+                   element[i].specificHeat,
+                   element[i].shells,
+                   element[i].valence);
+        }
+        else
+        {
+            printf("some kind of failure\n");
+        }
+    }
+};
+
+char *prefix[10] = {"mono", "di", "tri", "tetra", "petra", "hexa", "hepta", "octo", "nona", "deca"};
--- table.c
+++ table.c
@@ -3,18 +3,26 @@
 #include <string.h>
 #include "chemheader.h"
 
-int main(){
+int main()
+{
 
     table *element = buildTable("periodic_table.csv");
 
-    for(int j = 0; j < 118; j++){
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%s\n", prefix[i]);
+    }
+    // printtable(element);
+
+    // printElement("Hydrogen");
+
+    /*for(int j = 0; j < 118; j++){
         if(strcmp(element[j].element, "Sodium") == 0){
             printf("Sodium was discovered in %d by %s\n", element[j].year, element[j].discoverer);
         }
-    }
+    }*/
 
-    //Add le Cahtelier's Principle, equilibirum equation
+    // Add le Cahtelier's Principle, equilibirum equation
 
-    free(element);
-
+    // free(element);
 };
